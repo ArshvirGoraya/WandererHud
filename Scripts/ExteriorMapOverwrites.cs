0e7b16a885f1969f78de98ac2f75744f1cefb32d
Arshvir Goraya <113562877+ArshvirGoraya@users.noreply.github.com>
2025-01-01 15:59:52 -0500

    exterior map: dynamic resizing for aspect ratios

--- Scripts/ExteriorMapOverwrites.cs
+++ Scripts/ExteriorMapOverwrites.cs
@@ -7,6 +7,7 @@ using DaggerfallWorkshop.Game.UserInterfaceWindows;
 using System.Net.NetworkInformation;
 using System.Text.RegularExpressions;
 using System.Linq;
+using System.Collections;
 
 namespace ExteriorMapOverwritesMod
 {
@@ -16,6 +17,11 @@ namespace ExteriorMapOverwritesMod
 
         public DaggerfallExteriorAutomapWindow ExteriorMapWindow;
         public bool ButtonsDisabled = false;
+        public Vector2 LastScreen = new Vector2(0, 0);
+        public float ResizeWaitSecs = 0f;
+        public bool ResizeWaiting = false;
+        public Panel DummyPanelAutomap;
+        public Panel PanelRenderAutomap;
 
         [Invoke(StateManager.StateTypes.Start, 0)]
         public static void Init(InitParams initParams){
@@ -30,27 +36,61 @@ namespace ExteriorMapOverwritesMod
         private void Start(){
             DaggerfallUI.UIManager.OnWindowChange += UIManager_OnWindowChangeHandler;
             ExteriorMapWindow = DaggerfallUI.Instance.ExteriorAutomapWindow;
+            LastScreen.x = Screen.width;
+            LastScreen.y = Screen.height;
+       }
+
+        private void Update(){
+            if (DaggerfallUI.UIManager.TopWindow is DaggerfallExteriorAutomapWindow){
+                if (Screen.width != LastScreen.x || Screen.height != LastScreen.y){
+                    if (!ResizeWaiting){
+                        StartCoroutine(WaitSeconds(ResizeWaitSecs));
+                    }
+                }
+            }
+        }
+
+        private IEnumerator WaitSeconds(float seconds){
+            ForceResizeMap();
+            LastScreen.x = Screen.width; 
+            LastScreen.y = Screen.height;
+            ResizeWaiting = true;
+            yield return new WaitForSecondsRealtime(seconds);
+            ResizeWaiting = false;
+        }
+
+        public void ForceResizeMap(){
+            if (DaggerfallUI.UIManager.TopWindow is DaggerfallExteriorAutomapWindow){
+                Debug.Log($"updating map size");
+                // ExteriorMapWindow.Update();
+                // ExteriorMapWindow.UpdateAutomapView();
+                // ExteriorMapWindow.OnPop();
+                // ExteriorMapWindow.OnPush();
+                // ExteriorMapWindow.Draw(); // ! cant call outside of an GUI function
+                // exteriorAutomap.UpdateAutomapStateOnWindowPush()
+
+                ExteriorMapWindow.NativePanel.Size = ExteriorMapWindow.ParentPanel.Rectangle.size;
+                // DummyPanelAutomap.Size = ExteriorMapWindow.ParentPanel.Rectangle.size;
+                // PanelRenderAutomap.Size = ExteriorMapWindow.ParentPanel.Rectangle.size;
+                // Debug.Log($"WandererHud ExteriorMap: ParentPanel.Rectangle: {ExteriorMapWindow.ParentPanel.Rectangle.width}"); // ! Call component.Rectangle for autosize to apply!
+                // Debug.Log($"WandererHud ExteriorMap: NativePanel.Rectangle: {ExteriorMapWindow.NativePanel.Rectangle.width}"); // ! Call component.Rectangle for autosize to apply!
+                // Debug.Log($"WandererHud ExteriorMap: DummyPanelAutomap.Rectangle: {DummyPanelAutomap.Rectangle.width}"); // ! Call component.Rectangle for autosize to apply!
+                // Debug.Log($"WandererHud ExteriorMap: PanelRenderAutomap.Rectangle: {PanelRenderAutomap.Rectangle.width}"); // ! Call component.Rectangle for autosize to apply!
+            }
         }
 
         public void DisableComponents(){
             foreach (BaseScreenComponent component in ExteriorMapWindow.NativePanel.Components){
-                if (component is Button || component is HUDCompass){ // 
+                if (component is Button || component is HUDCompass){ // * Buttons and Compass Texture
                     component.Enabled = false;
                     continue;
                 }
                 // TODO: find better way to select these panels?
                 if (component is Panel && $"{component.Size}".Equals("(320.0, 10.0)")){ // * panelCaption (map legend)
-                    // foreach (BaseScreenComponent SubComponent in (component as Panel).Components){ // * Individual Legend components (temple/shop/tavern)
-                    //     SubComponent.Size = new Vector2(0, 0);
-                    //     SubComponent.Enabled = false;
-                    // }
-                    // component.Size = new Vector2(0, 0);
                     component.Enabled = false;
                     continue;
                 }
                 if (component is Panel && $"{component.Size}".Equals("(76.0, 17.0)")){ // * is dummyPanelCompass (controls click function like a button)
-                    // component.BackgroundColor = Color.blue;
-                    // component.Size = new Vector2(0, 0);
                     component.Enabled = false;
                     continue;
                 }
@@ -59,15 +99,31 @@ namespace ExteriorMapOverwritesMod
 
         public void ResizeMapPanels(){
             ExteriorMapWindow.NativePanel.AutoSize = AutoSizeModes.ResizeToFill;
-            Debug.Log($"WandererHud ExteriorMap: NativePanel.Rectangle: {ExteriorMapWindow.NativePanel.Rectangle.width}"); // ! Call component.Rectangle for autosize to apply!
+            Debug.Log($"WandererHud ExteriorMap Resize: NativePanel.Rectangle: {ExteriorMapWindow.NativePanel.Rectangle.width}"); // ! Call component.Rectangle for autosize to apply!
+
+            foreach (BaseScreenComponent component in ExteriorMapWindow.ParentPanel.Components){
+                if (component.Enabled && component is Panel){
+                    // Debug.Log($"component size: {component.Size}");
+                    // Debug.Log($"screen size: {Screen.width}, {Screen.height}");
+                    if (!$"{component.Size}".Equals($"({Screen.width}, {Screen.height})")){ // * panelRenderAutomap (scales with DummyPanelAutomap so doesn't matter)
+                        PanelRenderAutomap = component as Panel;
+                    }
+                }
+            }
 
             // TODO: find better way to select this panels?
             foreach (BaseScreenComponent component in ExteriorMapWindow.NativePanel.Components){
                 if (component.Enabled && component is Panel){
-                    if ($"{component.Size}".Equals("(318.0, 169.0)")){ // * is dummyPanelAutomap: buildingNamePlates
-                        // component.AutoSize = AutoSizeModes.ResizeToFill;
-                        component.AutoSize = AutoSizeModes.ScaleToFit;
-                        Debug.Log($"WandererHud ExteriorMap: dummyPanelAutomap.Rectangle: {component.Rectangle.width}"); // ! Call component.Rectangle for autosize to apply!
+                    if ($"{component.Size}".Equals("(318.0, 169.0)")){ // * is DummyPanelAutomap: buildingNamePlates
+                        DummyPanelAutomap = component as Panel;
+                        // component.AutoSize = AutoSizeModes.ResizeToFill; // ! Stretches map texture
+                        // component.AutoSize = AutoSizeModes.ScaleToFit; // ! Has bars on the sides.
+                        // component.AutoSize = AutoSizeModes.Scale; // ! Doesn't scale up
+                        // component.AutoSize = AutoSizeModes.None; // ! Doesn't scale up
+                        component.AutoSize = AutoSizeModes.ScaleFreely; // * Scales perfectly, without bars on side and without stretching the texture.
+                        component.HorizontalAlignment = HorizontalAlignment.Center;
+                        component.VerticalAlignment = VerticalAlignment.Middle;
+                        Debug.Log($"WandererHud ExteriorMap Resize: DummyPanelAutomap.Rectangle: {component.Rectangle.width}"); // ! Call component.Rectangle for autosize to apply!
                         // TODO: fix nameplates
                         // foreach (TextLabel SubComponent in ExteriorMapWindow.PanelRenderAutomap.Components.Cast<TextLabel>()){}
                     }
@@ -78,76 +134,28 @@ namespace ExteriorMapOverwritesMod
         }
 
         public void PrintDebug(){
-            // ExteriorMapWindow.ParentPanel
-                // 320.0, 200.0: native Panel
-                // 1144.8, 608.4: panelRenderAutomap (map texture)
-            // ExteriorMapWindow.NativePanel
-                // 318.0, 169.0: dummyPanelAutomap (nameplates)
-                // 320.0, 10.0: panelCaption (map legend)
-                // 76.0, 17.0: dummyPanelCompass (map click function)
+            // * ExteriorMapWindow.ParentPanel
+                // * 320.0, 200.0: native Panel
+                // * 1144.8, 608.4: panelRenderAutomap (map texture) Scales with DummyPanelAutomap.
+            // * ExteriorMapWindow.NativePanel
+                // * 318.0, 169.0: DummyPanelAutomap (nameplates)
+                // * 320.0, 10.0: panelCaption (map legend)
+                // * 76.0, 17.0: dummyPanelCompass (map click function)
             
-            // BaseComponentLog(ExteriorMapWindow.ParentPanel, "The parent component: ");
-            // foreach (BaseScreenComponent component in ExteriorMapWindow.ParentPanel.Components){ // * ParentPanel = StretchToFill (to entire screen)
-            //     if (component.Enabled && component is Panel){
-            //         if ($"{component.Size}".Equals("(320.0, 200.0)")){ // * Is NativePanel (ScaleToFit)
-            //             component.BackgroundTextureLayout = BackgroundLayout.StretchToFill;
-            //             component.AutoSize = AutoSizeModes.ResizeToFill;
-            //             Debug.Log($"WandererHud ExteriorMap new NativePanel.Rectangle: {component.Rectangle.width}"); // ! Need to call component.Rectangle for autosize to apply!
-            //         }
-            //         // if ($"{component.Size}".Equals("(1144.8, 608.4)")){ // * panelRenderAutomap (scales with dummyPanelAutomap so doesn't matter)
-            //         //     component.BackgroundColor = Color.magenta;
-            //         //     component.AutoSize = AutoSizeModes.ResizeToFill;
-            //         //     Debug.Log($"WandererHud ExteriorMap new Map Texture Panel rectangle: {component.Rectangle.width}"); // ! Need to call component.Rectangle for autosize to apply!
-            //         // }
-            //         // BaseComponentLog(component, "parent component: ");
-            //      }
-            // }    
+            foreach (BaseScreenComponent component in ExteriorMapWindow.ParentPanel.Components){
+                if ($"{component.Size}".Equals("(1144.8, 608.4)")){ // * panelRenderAutomap (scales with DummyPanelAutomap so doesn't matter)
+                    component.BackgroundColor = Color.magenta;
+                    BaseComponentLog(component, "panelRenderAutomap");
+                }
+            }
             foreach (BaseScreenComponent component in ExteriorMapWindow.NativePanel.Components){
                 if (component.Enabled && component is Panel){
-                    if ($"{component.Size}".Equals("(318.0, 169.0)")){ // * is dummyPanelAutomap: buildingNamePlates
+                    if ($"{component.Size}".Equals("(318.0, 169.0)")){ // * DummyPanelAutomap
                         // component.BackgroundColor = Color.green;
-                        component.AutoSize = AutoSizeModes.ResizeToFill;
-                        Debug.Log($"WandererHud ExteriorMap new dummyPanelAutomap.Rectangle: {component.Rectangle.width}"); // ! Need to call component.Rectangle for autosize to apply!
-                        // component.Size = new Vector2(0, 0);
-
-                        foreach (TextLabel SubComponent in ExteriorMapWindow.PanelRenderAutomap.Components.Cast<TextLabel>())
-                        {
-                            // ! Nothing you do here matters! It gets reset on `UpdateAutomapView()` anyway...
-                             // * All NamePlates Labels
-                            // SubComponent.RectRestrictedRenderArea = component.Rectangle;
-                            // SubComponent.RestrictedRenderAreaCustomParent = component as Panel;
-                            // SubComponent.TextScale *= 8;
-                            // SubComponent.Scale *= 8;
-                            // SubComponent.Position = Vector2.zero;
-
-                            Debug.Log($"TextLabel: {SubComponent}");
-                            Debug.Log($"| Position: {SubComponent.Position}");
-                            Debug.Log($"| TextScale: {SubComponent.TextScale}");
-                            // Debug.Log($"| UseRestrictedRenderArea: {SubComponent.UseRestrictedRenderArea}");
-                            // Debug.Log($"| RestrictedRenderAreaCoordinateType: {SubComponent.RestrictedRenderAreaCoordinateType}");
-                            // Debug.Log($"| RectRestrictedRenderArea: {SubComponent.RectRestrictedRenderArea}");
-                            // Debug.Log($"| RestrictedRenderAreaCustomParent: {SubComponent.RestrictedRenderAreaCustomParent}");
-                            // Debug.Log($"| Enabled: {SubComponent.Enabled}");
-                            // Debug.Log($"| AutoSize: {SubComponent.AutoSize}");
-                            Debug.Log($"| LocalScale: {SubComponent.LocalScale}");
-                            Debug.Log($"| Scale: {SubComponent.Scale}");
-                            // Debug.Log($"| MinAutoScale: {SubComponent.MinAutoScale}");
-                            // Debug.Log( $"| MaxAutoScale: {SubComponent.MaxAutoScale}");
-                            Debug.Log($"| WandererHud ExteriorMap new TextLabel.Rectangle: {SubComponent.Rectangle.width}"); // ! Need to call component.Rectangle for autosize to apply!
-                            Debug.Log($"===");
-                            
-                            
-                            // BaseComponentLog(SubComponent, "TextLabel?: ");
-                        }
+                        BaseComponentLog(component, "DummyPanelAutomap");
                     }
-                    // 
-                    // BaseComponentLog(component, "native component: ");
-                 }
+                }
             }
-
-            Debug.Log($"WandererHud ExteriorMap new Map Texture Panel rectangle: {ExteriorMapWindow.PanelRenderAutomap.Rectangle.width}"); // ! Need to call component.Rectangle for autosize to apply!
-            ExteriorMapWindow.UpdateAutomapView();
-            Debug.Log($"WandererHud ExteriorMap new Map Texture Panel rectangle: {ExteriorMapWindow.PanelRenderAutomap.Rectangle.width}"); // ! Need to call component.Rectangle for autosize to apply!
         }
 
         public void UIManager_OnWindowChangeHandler(object sender, EventArgs e){
@@ -161,39 +169,23 @@ namespace ExteriorMapOverwritesMod
                 // PrintDebug();
             }
         }
-        // public void PrintAllComponents(Panel ParentPanel, string Prefix = ""){
-        //     foreach (BaseScreenComponent component in ParentPanel.Components){
-        //         if (component.Enabled){
-        //             if (component is TextLabel){ continue; }
-        //             if (component is Panel){
-        //                 BaseComponentLog(component, Prefix);
-        //                 Prefix += "\t"; // * translates as 1 character in the string.
-        //                 PrintAllComponents(component as Panel, Prefix);
-        //                 Debug.Log($"Prefix.Length: {Prefix.Length}");
-        //                 Prefix = Prefix.Remove(Prefix.Length - 1, 1);
-        //                 continue;
-        //             }
-        //             BaseComponentLog(component, Prefix);
-        //         }
-        //     }
-        // }
         public void BaseComponentLog(BaseScreenComponent component, string Prefix = ""){
             PrefixLog(Prefix, $"| Component: {component}");
-            // PrefixLog(Prefix, $"| Enabled: {component.Enabled}");
-            // PrefixLog(Prefix, $"| Size: {component.Size}");
-            // PrefixLog(Prefix, $"| AutoSize: {component.AutoSize}");
-            // PrefixLog(Prefix, $"| BackgroundColor: {component.BackgroundColor}");
-            // PrefixLog(Prefix, $"| BackgroundTexture: {component.BackgroundTexture}");
-            // PrefixLog(Prefix, $"| BackgroundTextureLayout: {component.BackgroundTextureLayout}");
-            // PrefixLog(Prefix, $"| BackgroundCroppedRect: {component.BackgroundCroppedRect}");
-            // PrefixLog(Prefix, $"| HorizontalAlignment: {component.HorizontalAlignment}");
-            // PrefixLog(Prefix, $"| VerticalAlignment: {component.VerticalAlignment}");
-            // PrefixLog(Prefix, $"| UseRestrictedRenderArea: {component.UseRestrictedRenderArea}");
-            // PrefixLog(Prefix, $"| MinAutoScale: {component.MinAutoScale}");
-            // PrefixLog(Prefix, $"| MaxAutoScale: {component.MaxAutoScale}");
-            // PrefixLog(Prefix, $"| Scale: {component.Scale}");
-            // PrefixLog(Prefix, $"| LocalScale: {component.LocalScale}");
-            // PrefixLog(Prefix, $"===");
+            PrefixLog(Prefix, $"| Enabled: {component.Enabled}");
+            PrefixLog(Prefix, $"| Size: {component.Size}");
+            PrefixLog(Prefix, $"| AutoSize: {component.AutoSize}");
+            PrefixLog(Prefix, $"| BackgroundColor: {component.BackgroundColor}");
+            PrefixLog(Prefix, $"| BackgroundTexture: {component.BackgroundTexture}");
+            PrefixLog(Prefix, $"| BackgroundTextureLayout: {component.BackgroundTextureLayout}");
+            PrefixLog(Prefix, $"| BackgroundCroppedRect: {component.BackgroundCroppedRect}");
+            PrefixLog(Prefix, $"| HorizontalAlignment: {component.HorizontalAlignment}");
+            PrefixLog(Prefix, $"| VerticalAlignment: {component.VerticalAlignment}");
+            PrefixLog(Prefix, $"| UseRestrictedRenderArea: {component.UseRestrictedRenderArea}");
+            PrefixLog(Prefix, $"| MinAutoScale: {component.MinAutoScale}");
+            PrefixLog(Prefix, $"| MaxAutoScale: {component.MaxAutoScale}");
+            PrefixLog(Prefix, $"| Scale: {component.Scale}");
+            PrefixLog(Prefix, $"| LocalScale: {component.LocalScale}");
+            PrefixLog(Prefix, $"===");
         }
 
         public void PrefixLog(string Prefix, string LogComponent){
